4aa44e92903aadb9c764e751bcd0cb4d758b9888
redraider6381 <48606649+redraider6381@users.noreply.github.com>
2023-03-07 20:11:06 -0600

    Initial commit

---
+++ src/main/java/frc/robot/commands/Arm/ArmSuckCmd.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.Arm;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.ArmSubsystem;
+import java.util.function.Supplier;
+
+public class ArmSuckCmd extends CommandBase {
+
+    private final ArmSubsystem armSubsystem;
+
+    public ArmSuckCmd(ArmSubsystem armSubsystem) {
+        this.armSubsystem = armSubsystem;
+        addRequirements(armSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        armSubsystem.suck.set(0.2);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        armSubsystem.suck.set(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
